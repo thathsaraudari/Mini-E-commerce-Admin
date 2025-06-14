682c713f59bb50e7c110e2542494e416df8e7019
Noemí Mira <noemi.miranavarro@gmail.com>
2025-06-14 15:52:51 +0200

    first commit

---
+++ Mini-e-commerce-admin/src/App.jsx
@@ -0,0 +1,18 @@
+import elleLogo from './assets/logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <nav>
+          <img src={elleLogo} alt="logo" />
+          <h2>ELLE</h2>
+        </nav>
+      </div>
+
+    </>
+  )
+}
+
+export default App
