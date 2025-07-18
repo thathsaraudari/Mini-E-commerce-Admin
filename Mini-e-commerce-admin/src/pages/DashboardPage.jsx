d82e130e6f67bf14dfbd8ab4fc1bb7d2c2f026af
thathsaraudari <105314224+thathsaraudari@users.noreply.github.com>
2025-07-19 01:47:46 +0200

    Fix passing onDelete function

--- Mini-e-commerce-admin/src/pages/DashboardPage.jsx
+++ Mini-e-commerce-admin/src/pages/DashboardPage.jsx
@@ -1,12 +1,12 @@
 import AddProduct from '../components/AddProduct'
 import ProductList from '../components/ProductList'
 
-function DashboardPage({ products, handleDelete}) {
+function DashboardPage({ products, onDelete}) {
   return (
     <div>
       <AddProduct/>
       <h1>PRODUCTS LIST</h1>
-      <ProductList products={products} onDelete={handleDelete}/>
+      <ProductList products={products} onDelete={onDelete}/>
     </div>
   )
 }
