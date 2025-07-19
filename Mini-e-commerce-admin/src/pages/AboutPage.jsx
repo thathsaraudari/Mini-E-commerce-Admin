9082bb285093eb64a37b01eed9e492675481ad46
Noemí Mira <noemi.miranavarro@gmail.com>
2025-07-19 13:52:04 +0200

    new AboutPage

--- Mini-e-commerce-admin/src/pages/AboutPage.jsx
+++ Mini-e-commerce-admin/src/pages/AboutPage.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
 
 function AboutPage () {
   return (
-    <div>
+    <div style={{
+      padding: '40px',
+      maxWidth: '800px',
+      margin: '0 auto'
+    }}>
       <h1>ABOUT THE PROJECT</h1>
-      <p>This app shows one list of products whith details.</p>
-    </div>
-  )
-}
+      <h3 style={{color: "grey"}}>
+        DESCRIPTION: This project is a web application built by students as part of their coursework.
+      </h3>
 
-export default AboutPage
+      <h2 style={{color: "grey"}}>Meet the Team</h2>
+      <div style={{
+        border: '1px solid #ccc',
+        borderRadius: '8px',
+        padding: '15px',
+        marginBottom: '20px',
+        background: '#f3ebebff'
+      }}>
+        <h3 style={{ color: "grey"}}>Thathsara Abayawardana</h3>
+
+
+      </div>
+
+      <div style={{
+        border: '1px solid #ccc',
+        borderRadius: '8px',
+        padding: '15px',
+        marginBottom: '20px',
+        background: '#f3ebebff'
+      }}>
+        <h3 style={{ color: "grey"}}>Noemí Mira</h3>
+        <a href="https://github.com/NMN-germany" target="_blank" rel="noopener noreferrer" style={{ marginRight: '10px'}}>GitHub</a>
+        <a href="https://www.linkedin.com/in/noemi-mira-navarro/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
